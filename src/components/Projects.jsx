5ebeed1def0961429bddc7e2a8dcc1af96904075
Eng/Ibrahim Shahin <ibrahimsaaed38@gmail.com>
2025-05-24 23:43:52 +0300

    modify projects, skills

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,104 @@
-import React from 'react'
+import React from 'react';
 
 export default function Projects() {
   return (
-    <div className='d-flex gap-5 p-5'>
-      <h1 className='col-4 text-center' >Projects</h1>
-      <div className='d-flex gap-5 flex-column' >
-      <div>
-      <p>
-      Movies App, A front-end web application using React.js and Redux toolkit
-      The app allows users to view movies in different languages from the movie database API, search for their preferred movies, add movies to their wish list and display recommended movies.      </p>
-      <a href='https://movie-app-git-main-ahmed-fathalla-s-projects.vercel.app/' target='_blank' className='btn btn-dark'>Show Project</a>
-      </div>
-      <div>
-      <p>
-      Inventory Management System, A full-stack web application using Django framework
-      A centralized system that keeps track of shipments of products delivered to the warehouse from the factory, and products delivered to supermarkets (orders).
-      </p>
-      <a href='https://salahethaer.pythonanywhere.com/dashboard/' target='_blank' className='btn btn-dark'>Show Project</a>
-      </div>
-      <div>
-      <p>
-      Technify Electronics Store Designed an e-commerce web application for electronics shopping. Technologies: HTML5, CSS3, Bootstrap, JavaScript, jQuery.
-      </p>
-      <a href='https://nouranelshazly.github.io/CST-Project/pages/home.html' target='_blank' className='btn btn-dark'>Show Project</a>
-      </div>
-      <div>
-      <p>
-      News Application with React.js Built an application to fetch and display the latest news, gold prices, and currency exchange rates using appropriate APIs.      </p>
-      <a href='https://drive.google.com/file/d/1fXV36kPQVsEZkMbzQnw0iCn4gpWWw40K/view?usp=sharing' target='_blank' className='btn btn-dark'>Show Project</a>
-      </div>
+    <div className="d-flex gap-5 p-5">
+      <h1 className="col-4 text-center">Projects</h1>
+      <div className="d-flex gap-5 flex-column">
+        <div>
+          <p>
+            The Virtual Book Fair is an online platform designed to connect
+            publishers, authors, and readers in a seamless digital environment.
+            It offers three main interfaces: Admin Dashboard, Publisher
+            Dashboard, and User Website, each providing specialized
+            functionalities to manage accounts, books, sales, and user
+            interactions efficiently.
+          </p>
+          <a
+            href="https://reader-hub-ui.vercel.app/"
+            target="_blank"
+            className="btn btn-dark"
+          >
+            Show Project
+          </a>
+        </div>
+        <div>
+          <p>
+            A hotel reservation full-stack web app built with Django and React
+            enables users to browse, book, and manage hotel reservations
+            seamlessly. handles secure user authentication, database management
+            (e.g., rooms, bookings, and availability), and API endpoints for
+            CRUD operations.provides a dynamic, responsive interface with
+            components for searching hotels, viewing room details, selecting
+            dates, and confirming bookings.
+          </p>
+          <a
+            href="https://hotels-reservation-system-ui.vercel.app/"
+            target="_blank"
+            className="btn btn-dark"
+          >
+            Show Project
+          </a>
+        </div>
+        <div>
+          <p>
+            Movies App, A front-end web application using React.js and Redux
+            toolkit The app allows users to view movies in different languages
+            from the movie database API, search for their preferred movies, add
+            movies to their wish list and display recommended movies.{' '}
+          </p>
+          <a
+            href="https://movie-app-git-main-ahmed-fathalla-s-projects.vercel.app/"
+            target="_blank"
+            className="btn btn-dark"
+          >
+            Show Project
+          </a>
+        </div>
+        <div>
+          <p>
+            Inventory Management System, A full-stack web application using
+            Django framework A centralized system that keeps track of shipments
+            of products delivered to the warehouse from the factory, and
+            products delivered to supermarkets (orders).
+          </p>
+          <a
+            href="https://www.linkedin.com/posts/ibrahim-saeed-shahin_django-webdevelopment-inventorymanagement-activity-7310391286845370368-vnYj?utm_source=share&utm_medium=member_desktop&rcm=ACoAADvZjcUBmp3cRiQ2LWzDqusaLR7hdXxnQ-M"
+            target="_blank"
+            className="btn btn-dark"
+          >
+            Show Project
+          </a>
+        </div>
+        <div>
+          <p>
+            Technify Electronics Store Designed an e-commerce web application
+            for electronics shopping. Technologies: HTML5, CSS3, Bootstrap,
+            JavaScript, jQuery.
+          </p>
+          <a
+            href="https://nouranelshazly.github.io/CST-Project/pages/home.html"
+            target="_blank"
+            className="btn btn-dark"
+          >
+            Show Project
+          </a>
+        </div>
+        <div>
+          <p>
+            News Application with React.js Built an application to fetch and
+            display the latest news, gold prices, and currency exchange rates
+            using appropriate APIs.{' '}
+          </p>
+          <a
+            href="https://drive.google.com/file/d/1fXV36kPQVsEZkMbzQnw0iCn4gpWWw40K/view?usp=sharing"
+            target="_blank"
+            className="btn btn-dark"
+          >
+            Show Project
+          </a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,45 +1,103 @@
-import React from 'react'
-import '../styles/Skills.css'
+import React from 'react';
+import '../styles/Skills.css';
 const Skills = () => {
   return (
     <>
-    <h1 className='text-center'>Skills</h1>
-        <div className="field">
-            <div className="skill"><i className="fas fa-database"></i> MySQL</div>
-            <div className="skill"><i className="fas fa-database"></i> PostgreSQL</div>
+      <h1 className="text-center">Skills</h1>
+      <div className="field">
+        <div className="skill">
+          <i className="fas fa-database"></i> MySQL
         </div>
-        <div className="field">
-            <div className="skill"><i className="fab fa-github"></i> GitHub</div>
-            <div className="skill"><i className="fab fa-docker"></i> Docker</div>
-            <div className="skill"><i className="fab fa-git"></i> Git</div>
+        <div className="skill">
+          <i className="fas fa-database"></i> PostgreSQL
         </div>
-        <div className="field">
-            <div className="skill"><i className="fab fa-html5"></i> HTML</div>
-            <div className="skill"><i className="fab fa-css3-alt"></i> CSS</div>
-            <div className="skill"><i className="fab fa-js"></i> JavaScript</div>
-            <div className="skill"><i className="fab fa-react"></i> React</div>
-            <div className="skill"><i className="fab fa-bootstrap"></i> Bootstrap</div>
+        <div className="skill">
+          <i className="fas fa-leaf"></i> MongoDB
         </div>
-        <div className="field">
-            <div className="skill"><i className="fab fa-python"></i> Python</div>
-            <div className="skill"><i className="fab fa-code"></i> Django</div>
-            <div className="skill"><i className="fas fa-flask"></i> Flask</div>
-            <div className="skill"><i className="fab fa-node"></i> Node.js</div>
-            <div className="skill"><i className="fab fa-cogs"></i> Odoo</div>
+      </div>
+      <div className="field">
+        <div className="skill">
+          <i
+            className="fab f
+a-html5"
+          ></i>{' '}
+          HTML
         </div>
-        <div className="field">
-            <div className="skill"><i className="fas fa-brain"></i> Machine learning</div>
-            <div className="skill"><i className="fas fa-chart-line"></i> Data Analysis</div>
-            <div className="skill"><i className="fas fa-table"></i> Excel</div>
-            <div className="skill"><i className="fas fa-chart-bar"></i> Power BI</div>
-            <div className="skill"><i className="fas fa-chart-pie"></i> Tableau</div>
+        <div className="skill">
+          <i className="fab fa-css3-alt"></i> CSS
         </div>
-
-
-
-      
+        <div className="skill">
+          <i className="fab fa-js"></i> JavaScript
+        </div>
+        <div className="skill">
+          <i className="fab fa-js"></i> JQuery
+        </div>
+        <div className="skill">
+          <i className="fab fa-react"></i> React
+        </div>
+        <div className="skill">
+          <i className="fas fa-code"></i> Next Js
+        </div>
+        <div className="skill">
+          <i className="fas fa-paint-brush" aria-hidden="true"></i> Material-UI
+        </div>
+        <div className="skill">
+          <i className="fab fa-bootstrap"></i> Bootstrap
+        </div>
+      </div>
+      <div className="field">
+        <div className="skill">
+          <i className="fab fa-python"></i> Python
+        </div>
+        <div className="skill">
+          <i className="fab fa-code"></i> Django
+        </div>
+        <div className="skill">
+          <i className="fas fa-flask"></i> Flask
+        </div>
+        <div className="skill">
+          <i className="fab fa-node"></i> Node.js
+        </div>
+        <div className="skill">
+          <i className="fas fa-server"></i> Nest.js
+        </div>
+        <div className="skill">
+          <i className="fas fa-cogs"></i> Odoo
+        </div>
+      </div>{' '}
+      <div className="field">
+        <div className="skill">
+          <i className="fab fa-github"></i> GitHub
+        </div>
+        <div className="skill">
+          <i className="fab fa-bitbucket"></i> BitBucket
+        </div>
+        <div className="skill">
+          <i className="fab fa-docker"></i> Docker
+        </div>
+        <div className="skill">
+          <i className="fab fa-git"></i> Git
+        </div>
+      </div>
+      <div className="field">
+        <div className="skill">
+          <i className="fas fa-brain"></i> Machine learning
+        </div>
+        <div className="skill">
+          <i className="fas fa-chart-line"></i> Data Analysis
+        </div>
+        <div className="skill">
+          <i className="fas fa-table"></i> Excel
+        </div>
+        <div className="skill">
+          <i className="fas fa-chart-bar"></i> Power BI
+        </div>
+        <div className="skill">
+          <i className="fas fa-chart-pie"></i> Tableau
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
