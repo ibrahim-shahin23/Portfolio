163eb5e6b1fd6c63b60f7883b45e2eaf918ec85c
ibrahim-shahin23 <ibrahimsaaed38@gmail.com>
2025-09-03 21:31:18 +0300

    update projects list

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,12 @@ const About = () => {
       <h1 className='col-4 text-center' >About me</h1>
       <div>
       <p>
-      Software Engineer with a Bachelor’s in Computers and Automatic Control Engineering. Developed practical software engineering skills during a 4-month training at Information Technology Institute (ITI), focusing on designing and implementing software solutions. Strengthened expertise in AI through a Huawei internship, mastering Python, machine learning, deep learning, and AI frameworks. Skilled in SQL, Excel, Power BI, and Tableau from my internship at the National Telecommunication Institute (NTI), where I built interactive dashboards. Passionate about blending software development with AI to create innovative solutions.
-      </p>
+Dynamic Full Stack Engineer and Odoo Developer with experience developing and customizing Odoo ERP
+modules, alongside building real-time fleet management software using Nest.js, Next.js, Socket.IO, MongoDB.
+Proficient in Python, Django, Flask, React, and database management with MySQL and PostgreSQL, complemented
+by hands-on expertise in AI, machine learning, and data analysis from internships at Huawei and NTI. Skilled in
+creating responsive web applications and interactive dashboards, I am passionate about integrating AI with software
+systems to deliver innovative, smart solutions that optimize business processes and enhance user experiences.      </p>
       <a href='https://drive.google.com/file/d/19gMEIYDP2KW0FqIq84SkuRRTr7bzjf84/view?usp=sharing' target='_blank' className='btn btn-dark'>Download My CV</a>
       </div>
     </div>
